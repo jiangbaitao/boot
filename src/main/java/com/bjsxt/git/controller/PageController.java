7bba8c716ec48689e105f2d44ee67f7d8b6d5396
dream life <1427145894@qq.com>
2020-10-23 15:52:04 +0800

    项目经理第一次

---
+++ src/main/java/com/bjsxt/git/controller/PageController.java
@@ -0,0 +1,12 @@
+package com.bjsxt.git.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PageController {
+    @RequestMapping("/demo")
+    public String demo(){
+        return "aaa";
+    }
+}
